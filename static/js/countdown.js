3883937319bd4aebe5903d5b4a4d3d8d160ff257
desainimit <210133042048@silveroakuni.ac.in>
2023-02-02 23:47:43 +0530

    Full website done

--- static/js/countdown.js
+++ static/js/countdown.js
@@ -138,8 +138,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
+    document.body.scrollTop > 30 ||
+    document.documentElement.scrollTop > 30
   ) {
     mybutton.style.display = "block";
   } else {
